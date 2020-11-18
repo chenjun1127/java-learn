0d3787c524c5175f7ff45872ecfa6d6e23ab7d85
chenjun <402074940@qq.com>
2020-11-18 09:00:52 +0800

    raname file

--- java-base/src/com/thread/CallableTest.java
+++ java-base/src/com/thread/CallableTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.FutureTask;
  * @Author: chenjun
  * @Date: 2020/11/17 17:14
  */
-public class MyCallable implements Callable<String> {
+public class CallableTest implements Callable<String> {
     @Override
     public String call() throws Exception {
         try {
@@ -23,7 +23,7 @@ public class MyCallable implements Callable<String> {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 可以获取当前线程返回结果
-        FutureTask<String> futureTask = new FutureTask<>(new MyCallable());
+        FutureTask<String> futureTask = new FutureTask<>(new CallableTest());
         new Thread(futureTask).start();
         // 等待线程执行完毕获取返回结果
         System.out.println("result：" + futureTask.get());
